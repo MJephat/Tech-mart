a7488627090b42ba9f7701b5226f4e32175ebd51
MJephat <mukundijephat22@gmail.com>
2023-05-05 01:20:20 +0300

    designed Footer

--- e-com/src/components/ElectronicsList.js
+++ e-com/src/components/ElectronicsList.js
@@ -1,48 +1,78 @@
 import React, { useState, useEffect } from 'react';
 import SearchBar from './SearchBar';
 import Cart from './Cart';
+import CategoryFilter from './Category';
+
+function ElectronicsList() {
+  const [electronics, setElectronics] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [selectedCategory, setSelectedCategory] = useState('');
+  const [cart, setCart] = useState({ count: 0, items: [] });
+
+  useEffect(() => {
+    fetch('https://t-mart-data.onrender.com/electronics')
+      .then((response) => response.json())
+      .then((data) => setElectronics(data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  const addToCart = (item) => {
+    setCart((prevCart) => ({
+      count: prevCart.count + 1,
+      items: [...prevCart.items, item],
+    }));
+  };
+
+  const viewCart = () => {
+    alert(`Cart items: ${cart.items.map((item) => item.name).join(', ')}`);
+  };
+
+  const handleSearch = (query) => {
+    setSearchQuery(query);
+  };
+
+  const handleCategoryChange = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+
+  const removeItem = (item) => {
+    setCart((prevCart) => ({
+      count: prevCart.count - 1,
+      items: prevCart.items.filter((cartItem) => cartItem.id !== item.id),
+    }));
+  };
+
+  const categories = [...new Set(electronics.map((item) => item.category))];
+  const filteredElectronics = electronics.filter(
+    (item) =>
+      item.name.toLowerCase().includes(searchQuery.toLowerCase()) &&
+      (selectedCategory === '' || item.category === selectedCategory)
+  );
 
 
-function ElectronicsList(){
-    const [electronics, setElectronics] = useState([]);
-    const [searchQuery, setSearchQuery] = useState('');
-    const [cart, setCart] = useState({ count: 0, items: [] });
-    useEffect(() => {
-        fetch("https://t-mart-data.onrender.com/electronics")
-          .then((response) => response.json())
-          .then((data) => setElectronics(data))
-          .catch((error) => console.log(error));
-      }, []);
-      const addToCart = (item) => {
-        setCart((prevCart) => ({
-            count: prevCart.count + 1,
-            items: [...prevCart.items, item],
-        }));
-    };
-    const viewCart = () => {
-        alert(`Cart items: ${cart.items.map((item) => item.name).join(", ")}`);
-    };
-    const handleSearch = (query) => {
-        setSearchQuery(query);
-    };
-    const filteredElectronics = electronics.filter((item) => item.name.toLowerCase().includes(searchQuery.toLowerCase()));
-    return (
-        <div>
-            <SearchBar onSearch={handleSearch} />
-            <h2> TechMart Electonics </h2>
-            {filteredElectronics.map((item) => (
-                <div key={item.id} className="electrons">
-                    <h3>{item.name}</h3>
-                    <img src={item.image_url} alt={`${item.name}'s avatar`} />
-                    <p>Description: {item.description}</p>
-                    <p>Price: {item.price_ksh}</p>
-                    <p>Category: {item.category}</p>
-                    <p>Condition: {item.condition}</p>
-                    <button onClick={() => addToCart(item)}>Add to Cart</button>
-                </div>
-            ))}
-            <Cart cartItems={cart.items} onClick={viewCart} />
+  return (
+    <div className='panga'>
+      <SearchBar onSearch={handleSearch} />
+      <CategoryFilter
+        categories={categories}
+        selectedCategory={selectedCategory}
+        onCategoryChange={handleCategoryChange}
+      />
+      <h2> TechMart Electronics </h2>
+      {filteredElectronics.map((item) => (
+        <div key={item.id} className="electrons">
+          <h3>{item.name}</h3>
+          <img src={item.image_url} alt={`${item.name}'s avatar`} />
+          <p>Description: {item.description}</p><br></br>
+          <p><b>Price</b>:Ksh {item.price_ksh}</p><br></br>
+          <p><b>Category</b>: {item.category}</p>{" "}
+          <p><b>Condition</b>: {item.condition}</p><br></br>
+          <button onClick={() => addToCart(item)}>Add to Cart</button>
         </div>
-    );
+      ))}
+      <Cart cartItems={cart.items} onClick={viewCart} onRemove={removeItem} />
+    </div>
+  );
 }
 export default ElectronicsList;
+
--- e-com/src/components/Footer.js
+++ e-com/src/components/Footer.js
@@ -1,45 +1,48 @@
 import React from 'react';
 
+
 function Footer() {
   return (
     <footer>
-      <header>
-        <ul className="social-media-links">
-          <li><a href="#"><i className="fa fa-facebook"></i></a></li>
-          <li><a href="#"><i className="fa fa-twitter"></i></a></li>
-          <li><a href="#"><i className="fa fa-instagram"></i></a></li>
-          <li><a href="#"><i className="fa fa-google"></i></a></li>
-          <li><a href="#"><i className="fa fa-github"></i></a></li>
-        </ul>
-      </header>
-      <div className="footer-container">
-        <div className="footer-column">
-          <h3>Description</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae justo ac nibh dictum eleifend. Aliquam bibendum metus eu libero porta, ac ultricies lectus hendrerit.</p>
-        </div>
-        <div className="footer-column">
-          <h3>Products</h3>
-          <ul>
-            <li>Product 1</li>
-            <li>Product 2</li>
-            <li>Product 3</li>
-          </ul>
-        </div>
-        <div className="footer-column">
-          <h3>Useful Links</h3>
-          <ul>
-            <li><a href="#">Link 1</a></li>
-            <li><a href="#">Link 2</a></li>
-            <li><a href="#">Link 3</a></li>
-          </ul>
-        </div>
-        <div className="footer-column">
-          <h3>Contacts</h3>
-          <p>123 Main St<br/>City, State ZIP<br/>Phone: (123) 456-7890<br/>Email: info@example.com</p>
-        </div>
+        <div className="footer">
+            <div className="container">
+                <div className="row">
+                    <div className="footer-col">
+                        <h4>CONTACT US</h4>
+                        <ul>
+                            <li><a href="#">123,Victoria Plaza</a></li>
+                            <li><a href="#">City,Nairobi</a></li>
+                            <li><a href="#">Call 0789999990</a></li>
+                            <li><a href="#">techmart@yahoo.com</a></li>
+                        </ul>
+                    </div>
+                    <div className="footer-col">
+                        <h4>ABOUT US</h4>
+                        <ul>
+                            <li><a href="#">Terms of Service</a></li>
+                            <li><a href="#">Privacy and Conditions</a></li>
+                            <li><a href="#">Help</a></li>
+                            <li><a href="#">FAQ</a></li>
+                        </ul>
+                    </div>
+                    <div className="footer-col">
+                        <h4>FOLLOW US</h4>
+                        <div class="social-links">
+                            <a href="#">< i class="fab fa-facebook-f"></i></a>
+                            <a href="#">< i class="fab fa-twitter"></i></a>
+                            <a href="#">< i class="fab fa-instagram"></i></a>
+                            <a href="#">< i class="fab fa-github"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div className="new">
+                       <p className="col-sm">
+                        &copy;{new Date().getFullYear()} TECH-MART |All Rights Reserved |Terms Of Service |
+                       </p>
       </div>
+            </div>
+        </div>
     </footer>
   );
 }
-
 export default Footer;
