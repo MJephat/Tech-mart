d82ae59aeac8492fd777c7b57311298dcc97c968
MJephat <mukundijephat22@gmail.com>
2023-05-05 01:40:41 +0300

    created search bar component

--- e-com/src/components/SearchBar.js
+++ e-com/src/components/SearchBar.js
@@ -3,14 +3,16 @@ import React, { useState } from 'react';
 
 function SearchBar({ onSearch }) {
     const [searchQuery, setSearchQuery] = useState('');
+    
     const handleSearch = (event) => {
         event.preventDefault();
         onSearch(searchQuery);
     };
     return (
         <form onSubmit={handleSearch}>
-            <input type="text" placeholder="Search by name" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
-            <button type="submit">Search</button>
+            <input id='query' type="text" placeholder="Search by name" value={searchQuery} 
+            onChange={(e) => setSearchQuery(e.target.value)} />
+            {/* <button className="submit"type="submit">Search</button> */}
         </form>
     );
 }
