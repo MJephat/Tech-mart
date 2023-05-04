632597d53a1da05c1381f920bb74481132a01d58
MJephat <mukundijephat22@gmail.com>
2023-05-05 01:19:02 +0300

    created data,js and category component

---
+++ e-com/src/components/data.js
@@ -0,0 +1,31 @@
+export const slide = [
+    {
+      image: "../images/slide/slide1.png",
+    },
+    { image: "../images/slide/slide2.png" },
+    { image: "../images/slide/slide3.png" },
+    { image: "../images/slide/slide4.png" },
+  ]
+  export const order = [
+    {
+      id: 1,
+      title: "Your Order",
+      desc: "There are many variations of passages of Lorem Ipsum available",
+    },
+    {
+      id: 2,
+      title: "Ready Your Order",
+      desc: "There are many variations of passages of Lorem Ipsum available",
+    },
+    {
+      id: 3,
+      title: "Packing Your Order",
+      desc: "There are many variations of passages of Lorem Ipsum available",
+    },
+    {
+      id: 4,
+      title: "And Deliver",
+      desc: "There are many variations of passages of Lorem Ipsum available",
+    },
+  ]
+  
